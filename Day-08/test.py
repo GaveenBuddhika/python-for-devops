eeb5067642a6811b87ad7dd2c6e6c209955dba37
Gaveen_Buddhika <buddhik-se20022@stu.kln.ac.lk>
2024-10-30 11:15:09 +0530

    add tuple example

---
+++ Day-08/test.py
@@ -0,0 +1,25 @@
+#lists
+
+s3_bucket_lists = ['s3_bucket1', 's3_bucket2', 's3_bucket3', 's3_bucket4', 's3_bucket5']
+
+print(s3_bucket_lists[0:3])
+
+s3_bucket_lists.append('s3_bucket6')
+print(s3_bucket_lists)
+
+s3_bucket_lists.remove('s3_bucket6')
+print(s3_bucket_lists)
+
+print(len(s3_bucket_lists))
+
+
+# tuple
+
+s3_bucket_tuple = ('s3_bucket1', 's3_bucket2', 's3_bucket3', 's3_bucket4')
+
+print(s3_bucket_tuple[0:3])
+
+
+
+print(len(s3_bucket_tuple))
+

875b2f7d6bffd67dd46fd7ba337c700a278f089e
Gaveen_Buddhika <buddhik-se20022@stu.kln.ac.lk>
2024-10-30 19:33:05 +0530

    list functions

--- Day-10/01-convert-string-to-list.py
+++ Day-10/01-convert-string-to-list.py
@@ -1,1 +1,15 @@
-folder_paths = input("Enter a list of folder paths separated by spaces: ").split()
+import os
+
+folders = input("Enter a list of folder paths separated by spaces: ").split()
+
+for folder in folders:
+
+    try:
+        files = os.listdir(folder)
+    except FileNotFoundError:
+        print(f"Error: {folder} not found")
+        continue
+    
+for file in files:
+    print(file)
+
